80f31933dc1da427c8acb492047b70b890e1d442
bleachjade <bleachjade@hotmail.com>
2020-12-12 21:06:42 +0700

    change route, add index page

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/Vue_about.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: '/about',
+    name: 'Vue_about',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/',
+    name: 'Index',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Index.vue')
   }
 ]
 
