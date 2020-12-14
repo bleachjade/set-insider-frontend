956196acb1f78d31be1243d3d368feff6867b2f6
thun <nathun.thun@hotmail.com>
2020-12-14 23:45:35 +0700

    update

--- src/views/Stock.vue
+++ src/views/Stock.vue
@@ -1,15 +1,91 @@
 <template>
   <div class="container" id="Stock">
-    <CandleStick/>
+    <CandleStick :series="series" :chartOptions="chartOptions"/>
   </div>
 </template>
 <script>
 import CandleStick from "@/components/CandleStick.vue";
+import dayjs from "dayjs";
+import axios from "axios";
 
 export default {
-  name: "Stock",
+  name: "stock",
   components: {
-    CandleStick
+    CandleStick,
+  },
+  data() {
+    return {
+      series: [],
+      chartOptions: {},
+      data: []
+
+    };
+  },
+  beforeMount() {
+    axios
+      .get("http://localhost:3001/stock/price?symbol=SCC")
+      .then(response => this.preparedChartData(response.data.result))
+      .catch(error => console.log(error));
+  },
+   methods: {
+    preparedChartData(stocks) {
+      stocks.forEach(stock => {
+        let date = stock.date.split("/");
+        date[0] = parseInt(date[0]) - 543;
+        stock.date = new Date(date.join("-"));
+      });
+      stocks.sort(function(a, b) {
+        return a.date - b.date;
+      });
+
+      let data = [];
+
+      stocks.forEach(stock => {
+        data.push({
+          x: new Date(stock.date),
+          y: [
+            stock.openPrice,
+            stock.highestPrice,
+            stock.lowestPrice,
+            stock.closePrice
+          ]
+        });
+      });
+
+      this.series = [
+        {
+          name: "candle",
+          data: data
+        }
+      ];
+
+      this.chartOptions = { 
+        chart: {
+          type: "candlestick",
+          height: 350
+        },
+        title: {
+          text: "CandleStick Chart",
+          align: "left"
+        },
+        tooltip: {
+          enabled: true
+        },
+        xaxis: {
+          type: 'category',
+          labels: {
+                formatter: function(val) {
+                  return dayjs(val).format('ddd DD MMM YY');
+              }
+          }
+        },
+        yaxis: {
+          tooltip: {
+            enabled: true
+          }
+        }
+      };
+    }
   }
 };
 </script>
