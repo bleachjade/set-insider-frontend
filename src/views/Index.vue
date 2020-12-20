16bb155be589e9381f6b831efc79dea69db55b66
bleachjade <bleachjade@hotmail.com>
2020-12-20 12:06:34 +0700

    delete unused banner

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="main-container">
 
-    <div class="banner-container">
-      <div class="banner-wrap">
-        <!-- <img src="../assets/18980.jpg" alt="banner img"> -->
-      </div>
-    </div>
-
     <div class="content-container">
       <div class="content-wrap">
         <h1>This is content part</h1>
@@ -18,16 +12,6 @@
   </div>
 </template>
 
-<style scope>
-.banner-container {
-  display: flex;
-}
-.banner-wrap {
-  display: flex;
-  flex-wrap: wrap;
-  width: 100%;
-}
-.banner-wrap img {
-  width: 100%;
-}
+<style scoped>
+
 </style>
