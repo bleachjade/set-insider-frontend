06aae8308d223876890572f89c5a834c9469033e
bleachjade <bleachjade@hotmail.com>
2020-12-20 12:05:55 +0700

    change / path name to Index

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ Vue.use(VueRouter)
 
 const routes = [{
         path: '/',
-        name: 'Vue_about',
+        name: 'Index',
         component: () =>
             import ('../views/Index.vue')
     },
